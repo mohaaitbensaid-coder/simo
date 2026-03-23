dd1061593b07ae29890be98cf9c89b1f35e82f0c
mohaaitbensaid-coder <mohaaitbensaid@example.com>
2026-03-23 01:04:55 +0100

    Add PWA support (manifest.json and service worker) to make it a mobile app

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'taroudant-bike-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
